8349e4b8fb5cc21a408373920d19676bb21085e4
taronveq <38757775+taronveq@users.noreply.github.com>
2023-07-25 19:23:41 +0400

    written unit tests

--- path/to/your/new_test.swift
+++ path/to/your/new_test.swift
@@ -1,28 +1,28 @@
 class GPTTestTest: XCTestCase {
 
-func testFun() {
-    let gptTest = GPTTest()
-    gptTest.fun()
-    XCTAssertEqual("gpt tested", parsedOutput)
-}
-
-func testNewFun() {
-    let gptTest = GPTTest()
-    let result = gptTest.newFun()
-    XCTAssertEqual(0, result)
-}
-
-var parsedOutput: String = ""
-override class func setUp() {
-    super.setUp()
-    let originalPrint = Swift.print
-    Swift.print = { items in
-        parsedOutput = "\(items)"
+    func testFun() {
+        let gptTest = GPTTest()
+        gptTest.fun()
+        XCTAssertEqual("gpt tested", parsedOutput)
     }
-}
 
-override class func tearDown() {
-    Swift.print = originalPrint
-    super.tearDown()
-}
+    func testNewFun() {
+        let gptTest = GPTTest()
+        let result = gptTest.newFun()
+        XCTAssertEqual(0, result)
+    }
+
+    var parsedOutput: String = ""
+    override class func setUp() {
+        super.setUp()
+        let originalPrint = Swift.print
+        Swift.print = { items in
+            parsedOutput = "\(items)"
+        }
+    }
+
+    override class func tearDown() {
+        Swift.print = originalPrint
+        super.tearDown()
+    }
 }
